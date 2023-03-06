fc65f1b041addbfce366a0cbc9858f4e7fffa3a7
SeikaHirori <SeikaHirori@outlook.com>
2023-03-06 15:07:19 -0800

    Initial Commit

---
+++ Animation/AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationApp.swift
+//  Animation
+//
+//  Created by Seika Hirori on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
