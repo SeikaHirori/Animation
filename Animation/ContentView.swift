bb3bb020378da35eea336737c45dcc4bd445c3a3
SeikaHirori <SeikaHirori@outlook.com>
2023-03-16 06:22:30 -0700

    test push

--- Animation/ContentView.swift
+++ Animation/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var hello_World: String = "Hello World :3"
+    @State private var hello_World: String = "Hello World :333"
     
     var body: some View {
         part_1_implementation(hello_World: $hello_World)
--- _docs/rfer.md
+++ _docs/rfer.md
@@ -1,3 +1,4 @@
 # Project 6
 
 test
+
