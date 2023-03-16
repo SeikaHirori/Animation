5a7e026b0a525bfcd4c34fd7093aed7ad4e4069f
SeikaHirori <SeikaHirori@outlook.com>
2023-03-16 06:07:43 -0700

    Setup separate struct views for each part of lessons

--- Animation/ContentView.swift
+++ Animation/ContentView.swift
@@ -8,19 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var hello_World: String = "Hello World :3"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        part_1_implementation(hello_World: $hello_World)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct part_1_implementation: View {
+    @Binding var hello_World:String
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text(hello_World)
+        }
+        .padding()
+    }
+}
+
+struct part_2_implementation: View {
+    @Binding var hello_World:String
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text(hello_World)
+        }
+        .padding()
     }
 }
