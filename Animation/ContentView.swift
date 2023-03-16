142c9581a13100dc133ce4f0c2e40c57aacfd158
SeikaHirori <SeikaHirori@outlook.com>
2023-03-16 08:14:08 -0700

    Added debug print for when the animation state increases

--- Animation/ContentView.swift
+++ Animation/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     @State private var hello_World: String = "Hello World :333"
     
+    
     var body: some View {
         part_1_implementation(hello_World: $hello_World)
     }
@@ -23,6 +24,7 @@ struct ContentView_Previews: PreviewProvider {
 
 struct part_1_implementation: View {
     @Binding var hello_World:String
+    @State private var animationAmount: Double = 1.0
     
     var body: some View {
         VStack {
@@ -30,9 +32,28 @@ struct part_1_implementation: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text(hello_World)
+            
+            Button("Tap Me") {
+                animationAmount += 1
+            }
+            .padding(50)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+            .onChange(of: animationAmount) { _ in
+                debug_print_animation_state(animationAmount: animationAmount)
+            }
+            
+            
         }
         .padding()
     }
+    
+    func debug_print_animation_state(animationAmount: Double) {
+        print("Animation amount: \(animationAmount)")
+    }
+
 }
 // test
 
@@ -47,5 +68,19 @@ struct part_2_implementation: View {
             Text(hello_World)
         }
         .padding()
+    }
+    
+}
+
+// RFER #1
+extension Binding {
+    func onChange(_ handler: @escaping (Value) -> Void) -> Binding<Value> {
+        Binding(
+            get: { self.wrappedValue },
+            set: { newValue in
+                self.wrappedValue = newValue
+                handler(newValue)
+            }
+        )
     }
 }
--- _docs/rfer.md
+++ _docs/rfer.md
@@ -1,4 +1,5 @@
 # Project 6
 
-
+- RFER #1 | debugging: observing change in variable state
+    - https://www.hackingwithswift.com/quick-start/swiftui/how-to-run-some-code-when-state-changes-using-onchange
 
